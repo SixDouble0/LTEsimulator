3bc2795fa5fdcd5b95fc983fac1c9c6159706d93
SixDouble0 <162843064+SixDouble0@users.noreply.github.com>
2025-07-22 18:38:05 +0200

    Update Scheduler.cpp

--- src/Scheduler.cpp
+++ src/Scheduler.cpp
@@ -6,7 +6,6 @@ std::vector<int> Scheduler::AllocateResources(const std::vector<int>& userIds) {
     std::vector<int> allocated;
     if (userIds.empty()) return allocated;
 
-    // Round Robin: przydziel zasób jednemu u¿ytkownikowi na turê
     allocated.push_back(userIds[lastIndex]);
     lastIndex = (lastIndex + 1) % userIds.size();
     return allocated;
